c966baaa39faa008585d87ad2c78d86aa918e9a5
Rajat <rajatbisoi1@gmail.com>
2025-08-04 18:42:46 +0530

    added react agent

---
+++ ReAct_agent.py
@@ -0,0 +1,81 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+def add(a: int, b:int)->int:
+    '''this function adds 2 numbers'''
+
+    return a+b
+
+def sub(a: int, b:int)->int:
+    '''This function substractes first arg with 2nd arg'''
+
+    return a - b
+
+tools= [add,sub]
+
+llma = ChatOllama(model='qwen2.5-coder:7b-instruct-q4_K_M').bind_tools(tools)
+
+def model_call(state: AgentState)->AgentState:
+    system_prompt = SystemMessage(content=
+                                  "You Are A very higly skiled multitaster Ai who read the human message very carefully and follow instruction step by step.")
+    response = llma.invoke([system_prompt] + state['messages'])
+    return {'messages':[response]}
+
+def should_continue(state: AgentState)->str:
+    messaages= state['messages']
+    last_message = messaages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    
+    else:
+        return "Continue"
+    
+
+#----------------Graph--------------------------
+
+graph = StateGraph(AgentState)
+graph.add_node('our_agent',model_call)
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools",tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    'our_agent',
+    should_continue,
+    {
+        "Continue":"tools",
+        'end': END
+    }
+)
+
+graph.add_edge('tools',"our_agent")
+
+app = graph.compile()
+
+#-----------------------------------------------
+
+def print_stream(stream):
+    for s in stream:
+        message = s['messages'][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {'messages': [('user', "add 4 and 5 then sub 9 and 2  then add both results")]}
+
+print_stream(app.stream(inputs, stream_mode='values'))
--- agent_bot.py
+++ agent_bot.py
@@ -12,14 +12,16 @@ class AgentState(TypedDict):
     messages: list[HumanMessage | AIMessage]
 
 # llm = ChatOpenAI(model='gpt-4o')
-llma = ChatOllama(model='gemma:2b')
+# llma = ChatOllama(model='gemma:2b')
+llma = ChatOllama(model='qwen2.5:0.5B')
 
 def process(state: AgentState)->AgentState:
-    '''
-    This Node will invke the mml for human message input`
+    '''̦
+    This Node will invoke the llm for human message input`
     '''
     response = llma.invoke(state['messages'])
     print('AI:', response.content)
+    conversation_history.append(AIMessage(content=response.content))
     return state
 
 graph= StateGraph(AgentState)
@@ -28,10 +30,30 @@ graph.add_edge(START, 'process')
 graph.add_edge('process', END)
 agent = graph.compile()
 conversation_history: list[HumanMessage | AIMessage] = []
+try:
+    with open('conversation_history.txt', 'r') as f:
+        for line in f:
+            if str(line).startswith('User:'):
+                conversation_history.append(f"User: {HumanMessage(content=line)}")
+            if str(line).startswith('AI:'):
+                conversation_history.append(f"User: {AIMessage(content=line)}")
+    print("Conversation history loaded from 'conversation_history.txt'.")
+except FileNotFoundError:
+    print('file not found')
+
+
 user_input = input('Enter: ')
-while True:
+while user_input != 'exit':
     conversation_history.append(HumanMessage(content=user_input))
     state =  AgentState({"messages": conversation_history})
     result = agent.invoke(state)
     conversation_history = result['messages']
     user_input = input('Enter: ')
+with open('conversation_history.txt', 'w') as f:
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            f.write(f"User: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            f.write(f"AI: {message.content}\n")
+print("Conversation history saved to 'conversation_history.txt'.")
+
